fd87039a9d554976c3d2b93cf83e83bf6f921e1a
Preet Kaler <preetkalerop@gmail.com>
2025-11-02 19:43:26 +0530

    Fixed Cors

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import backend  # your backend.py file
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/")
 def home():
